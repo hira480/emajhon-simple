378a66c165ae50f825419b71d340d60cf088bc6f
hira480 <hiramannan8@gmail.com>
2022-03-22 01:41:42 +0600

    evenhandelar added add to cart button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,7 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
