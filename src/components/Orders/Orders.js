eacab94493b1c9fa65dcdf4a58013e0eea814da2
hira480 <hiramannan8@gmail.com>
2022-04-03 20:04:15 +0600

    redirect file added for hander 404 error

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import { removeFromDb } from '../../utilities/fakedb';
@@ -8,7 +8,7 @@ import ReviewItems from '../ReviewItems/ReviewItems';
 import './Orders.css';
 
 const Orders = () => {
-    const [products, setProducts] = useProducts();
+    const [products,] = useProducts();
     const [cart, setCart] = useCart(products);
     const navigate = useNavigate();
 
